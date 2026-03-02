098a6618dbfa52942c7e505d738412e961c10724
phanindra <phani.zero@gmail.com>
2026-03-02 23:36:14 +0530

    two sum and max profit puzzles

---
+++ src/main/Java/DataStructures/Arrays/TwoSum.java
@@ -0,0 +1,36 @@
+package DataStructures.Arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+ */
+// TC --> O(n)
+// SC --> O(n)
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {3,2,4};
+        int target = 6;
+        int[] result = twoSum(arr,target);
+        System.out.println(Arrays.toString(result));
+
+
+    }
+    public static int[] twoSum(int[] arr, int target){
+        HashMap<Integer,Integer> hashmap = new HashMap<>();
+        int j=0,q=0;
+        for(int i=0; i < arr.length; i++){
+            int diff = target - arr[i];
+            if(hashmap.containsKey(diff)){
+                j=hashmap.get(diff);
+                q=i;
+            }
+            hashmap.put(arr[i],i);
+        }
+        System.out.println("j :"+j);
+        System.out.println("q :"+q);
+        return new int[]{j,q};
+    }
+}
---
+++ src/main/Java/DataStructures/Arrays/maxProfit.java
@@ -0,0 +1,40 @@
+package DataStructures.Arrays;
+
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+public class maxProfit {
+
+    public static void main(String[] args) {
+        int[] arr ={7,1,5,3,6,4};
+        int profit = maxProfit(arr);
+        System.out.println("-- maxProfit : "+profit);
+        int profit_bruteForce = maxProfit_byBruteForce(arr);
+        System.out.println("-- profit_bruteForce : "+profit_bruteForce);
+    }
+
+    public static int maxProfit(int[] arr){
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int p : arr){
+            min = Math.min(min,p);
+            profit = Math.max(profit,p-min);
+        }
+        return profit;
+    }
+
+    public  static int maxProfit_byBruteForce(int[] arr){
+        int profit =0;
+        int temp=0;
+        for(int i=0; i < arr.length ;i++){
+            for(int j =i+1; j < arr.length;  j++){
+                temp =arr[j] - arr[i];
+                if(temp > profit)
+                    profit = temp;
+            }
+        }
+        return  profit;
+    }
+}
