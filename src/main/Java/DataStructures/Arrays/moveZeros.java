40ab1907b261e9963128c1dc18e340e343c36fe0
phanindra <phani.zero@gmail.com>
2026-03-06 18:15:30 +0530

    moveZeros

--- src/main/Java/DataStructures/Arrays/moveZeros.java
+++ src/main/Java/DataStructures/Arrays/moveZeros.java
@@ -1,4 +1,30 @@
 package DataStructures.Arrays;
 
-public class moveZeros {
+import java.util.Arrays;
+
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+*/
+
+// TC -> O(n).  SC -> O(1)
+class  moveZeros {
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,0,8,5,1,4,2,0};
+        moveZeros(arr);
+    }
+    public  static  void moveZeros(int[] arr){
+        int j=0;
+        for(int i=0; i< arr.length ; i++){
+            if(arr[i] != 0){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    j++;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
 }
