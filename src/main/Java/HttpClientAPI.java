beac490712aca3ba61ed4cc3c98c42ee3be0847f
phanindra <phani.zero@gmail.com>
2026-04-06 15:56:31 +0530

    java built-in feature for api calls

--- src/main/Java/HttpClientAPI.java
+++ src/main/Java/HttpClientAPI.java
@@ -8,17 +8,18 @@ import java.net.http.HttpResponse;
 public class HttpClientAPI {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
         HttpClient client = HttpClient.newHttpClient();
         String json = "{ \"title\": \"foo\", \"body\": \"bar\", \"userId\": 1 }";
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
-                .header("Contenet-type","application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(json))
-                .build();
+                               .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                               .header("Contenet-type","application/json")
+                               .POST(HttpRequest.BodyPublishers.ofString(json))
+                               .build();
 
         HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
-        System.out.println("Status code : "+response.statusCode());
+        System.out.println("Status Code : "+response.statusCode());
         System.out.println("Response Body : "+response.body());
 
     }
--- src/main/Java/HttpUrlConnectionAPI.java
+++ src/main/Java/HttpUrlConnectionAPI.java
@@ -8,6 +8,7 @@ import java.net.URL;
 public class HttpUrlConnectionAPI {
     public static void main(String[] args) {
         try {
+
             URL url = new URL("https://jsonplaceholder.typicode.com/posts");
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             conn.setRequestMethod("POST");
@@ -16,6 +17,7 @@ public class HttpUrlConnectionAPI {
             conn.setDoOutput(true);
 
             String jsonInputString =  "{ \"title\": \"foo\", \"body\": \"bar\", \"userId\": 1 }";
+
             try(OutputStream os = conn.getOutputStream()){
                 byte[] input = jsonInputString.getBytes("utf-8");
                 os.write(input,0,input.length);
@@ -24,7 +26,7 @@ public class HttpUrlConnectionAPI {
             int responseCode = conn.getResponseCode();
             System.out.println("Response Code : "+responseCode);
 
-            // Read response
+            // Read response and print
             try(java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream(),"utf-8"))){
                 StringBuilder response = new StringBuilder();
                 String responseLine;
@@ -32,13 +34,10 @@ public class HttpUrlConnectionAPI {
                     response.append(responseLine.trim());
                 }
                 System.out.println("Response  : "+response);
-
             }
-
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
